9d6772371b4b40810a8c89ddafc1940697b682b7
Steven Tomcavage <steven@tomcavage.com>
2016-05-11 10:14:08 -0400

    Getting started

---
+++ hello.c
@@ -0,0 +1,27 @@
+#include <sys/param.h>
+#include <sys/module.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+
+static int load(struct module *module, int cmd, void *arg) 
+{
+	int error = 0;
+
+	switch (cmd) 
+	{
+		case MOD_LOAD:
+			uprintf("Hello, world!\n");
+			break;
+		case MOD_UNLOAD:
+			uprintf("Goobye, world!\n");
+		default:
+			error = EOPNOTSUPP;
+			break;
+	}
+
+	return(error);
+}
+
+static moduledata_t hello_mod = { "hello", load, NULL };
+
+DECLARE_MODULE(hello, hello_mod, SI_SUB_DRIVERS, SI_ORDER_MIDDLE);
